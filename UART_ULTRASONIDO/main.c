6b11e15a6a353bdcb036b3b4440f3640d57cb78b
Juan Manuel <mendoza.jm@pucp.pe>
2018-05-25 10:20:22 -0500

    Ultima modificacion main.c S. Ultrasonido

--- UART_ULTRASONIDO/main.c
+++ UART_ULTRASONIDO/main.c
@@ -43,7 +43,42 @@ void US_trigger(void){
 	for(n = 0; n < 100; n++);
 	GPIO_PORTF_DATA_R  &= ~(0x02) ;
 }
-//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////HERE ftoa()
+
+// reverses a string 'str' of length 'len'
+void reverse(char *str, int len)
+{
+    int i=0, j=len-1, temp;
+    while (i<j)
+    {
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+        i++; j--;
+    }
+}
+ 
+ // Converts a given integer x to string str[].  d is the number
+ // of digits required in output. If d is more than the number
+ // of digits in x, then 0s are added at the beginning.
+int intToStr(int x, char str[], int d)
+{
+    int i = 0;
+    while (x)
+    {
+        str[i++] = (x%10) + '0';
+        x = x/10;
+    }
+ 
+    // If number of digits required is more, then
+    // add 0s at the beginning
+    while (i < d)
+        str[i++] = '0';
+ 
+    reverse(str, i);
+    str[i] = '\0';
+    return i;
+}
 // Converts a floating point number to string. (char *res === array[])
 void ftoa(float n, char *res, int afterpoint)
 {
